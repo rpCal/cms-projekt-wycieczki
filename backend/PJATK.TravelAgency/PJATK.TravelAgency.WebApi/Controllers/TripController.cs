eb775730bf84c5d5c182fac71beadad755885dfe
Piotr Surdyn <piotrsurdyn@gmail.com>
2018-12-06 20:48:26 +0100

    added get methods to TripController

--- backend/PJATK.TravelAgency/PJATK.TravelAgency.WebApi/Controllers/TripController.cs
+++ backend/PJATK.TravelAgency/PJATK.TravelAgency.WebApi/Controllers/TripController.cs
@@ -1,20 +1,41 @@
-﻿using System;
+﻿using PJATK.TravelAgency.WebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Results;
+using Newtonsoft.Json;
 
 namespace PJATK.TravelAgency.WebApi.Controllers
 {
     [RoutePrefix("travelAgency/trips")]
     public class TripController : ApiController
     {
-        //[HttpGet]
-        //public JsonResult GetAllTrips()
-        //{
-        //    return new JsonResult("");
-        //}
+        private MyDbContext _context;
+
+        public TripController()
+        {
+            _context = new MyDbContext();
+        }
+
+        [HttpGet]
+        public JsonResult<List<Trip>> GetAllTrips()
+        {
+            var trips = _context.Trips.ToList();            
+
+            return Json(trips);
+        }
+
+        [HttpGet]
+        public JsonResult<Trip> GetTrip(Guid id)
+        {
+            var trip = _context.Trips.
+                Where(x => x.Id == id).
+                FirstOrDefault();
+
+            return Json(trip);
+        }
     }
 }
