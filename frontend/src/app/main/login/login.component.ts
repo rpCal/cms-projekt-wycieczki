0208d7cf4e4e36599418e9ecb2d93a921a339cdf
Jan Zawadka <janzavvadka@gmail.com>
2018-12-07 22:06:03 +0100

    login page with autentication

--- frontend/src/app/main/login/login.component.ts
+++ frontend/src/app/main/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { AuthenticationService } from '../../service-authentication/authentication.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+    loginForm: FormGroup;
+    loading = false;
+    returnUrl: string;
+    error = '';
 
-  constructor() { }
+    constructor(
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router,
+        private authenticationService: AuthenticationService) {}
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.loginForm = this.formBuilder.group({
+            email: ['', Validators.required],
+            password: ['', Validators.required]
+        });
+        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+    }
 
+    onSubmit() {
+        if (this.loginForm.invalid) {
+            return;
+        }
+        this.loading = true;    
+        this.authenticationService.login(this.loginForm.controls.email.value, this.loginForm.controls.password.value)
+            .subscribe(
+              data => {
+                this.loading = false;
+                this.router.navigate([this.returnUrl]);
+              },
+              error => {
+                this.error = error.message;
+                ;
+                this.loading = false;
+              });
+               
+    }
+
+    logout(){
+        this.authenticationService.logout();
+    }
 }
