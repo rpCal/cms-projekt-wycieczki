23c2a7a01178a7c0eed7411dd7bbc5013f7ea445
Jan Zawadka <janzavvadka@gmail.com>
2018-12-09 13:51:26 +0100

    constructor for trip

--- frontend/src/app/model/trip.ts
+++ frontend/src/app/model/trip.ts
@@ -11,4 +11,19 @@ export class Trip {
     availableNumberOfPlaces: number;
     archive: boolean;
     promote: number;
+
+    constructor(name: string, city: string, departureDate: Date, arrivalDate: Date, price: number, describe: string, departurePlace: string, numberOfPlaces: number, availableNumberOfPlaces: number, archive: boolean, promote: number) {
+    
+    this.name = name;
+    this.city = city;
+    this.departureDate = departureDate;
+    this.arrivalDate = arrivalDate;
+    this.price = price;
+    this.describe = describe;
+    this.departurePlace = departurePlace;
+    this.numberOfPlaces = numberOfPlaces;
+    this.availableNumberOfPlaces = availableNumberOfPlaces;
+    this.archive = archive;
+    this.promote = promote;
+    }
 }
