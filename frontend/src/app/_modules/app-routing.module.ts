ada274b17bfd36eb1549e620ad7554eb82b3920d
Jan Zawadka <janzavvadka@gmail.com>
2018-12-14 00:44:00 +0100

    structure wrap modules, add materials

--- frontend/src/app/_modules/app-routing.module.ts
+++ frontend/src/app/_modules/app-routing.module.ts
@@ -1,10 +1,10 @@
-import { AuthGuard } from './auth-guard/auth.guard';
-import { LoginComponent } from './main/login/login.component';
-import { TripListComponent } from './main/trip-list/trip-list.component';
+import { AuthGuard } from '../auth-guard/auth.guard';
+import { LoginComponent } from '../main/login/login.component';
+import { TripListComponent } from '../main/trip-list/trip-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { MainPageComponent } from './main/main-page/main-page.component';
-import { LastMinuteComponent } from './main/last-minute/last-minute.component';
+import { MainPageComponent } from '../main/main-page/main-page.component';
+import { LastMinuteComponent } from '../main/last-minute/last-minute.component';
 
 const routes: Routes = [
   { path: '', redirectTo: "main-page", pathMatch: "full"},
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { FakeBackendInterceptor } from './mocks/fake.backend';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './_modules/app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -15,6 +16,7 @@ import { LoginComponent } from './main/login/login.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BurgerComponent } from './nav/burger/burger.component';
 import { LastMinuteComponent } from './main/last-minute/last-minute.component';
+import { MaterialModule } from './_modules/material.module';
 
 export let fakeBackendProvider = {
   provide: HTTP_INTERCEPTORS,
@@ -39,7 +41,8 @@ export let fakeBackendProvider = {
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [
     //disconnet when real api
