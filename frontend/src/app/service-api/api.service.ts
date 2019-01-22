e6f42ed3a1ee56653ad517cf3b71cd48c68763d5
Jan Zawadka <janzavvadka@gmail.com>
2019-01-22 22:20:14 +0100

    new service api urls

--- frontend/src/app/service-api/api.service.ts
+++ frontend/src/app/service-api/api.service.ts
@@ -13,24 +13,23 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class ApiService {
-  private baseUrl = 'http://travelagencyapp.azurewebsites.net';
-
+  private baseUrl = 'http://pjatktravelagencywebapi20190118114614.azurewebsites.net/travelAgency/';
   constructor(private http: HttpClient) { }
 
   login(username, password){
-    return this.http.post<any>(this.baseUrl + `/users/authenticate`, { username, password });
+    return this.http.post<any>(this.baseUrl + `users/authenticate`, { username, password });
   }
 
   getTrips(): Observable<Trip[]> {
-    return this.http.get<Trip[]>(this.baseUrl + "/travelAgency/trips");
+    return this.http.get<Trip[]>(this.baseUrl + "trips");
   }
 
   getTrip(id: number): Observable<Trip> {
-    return this.http.get<Trip>(this.baseUrl + "/travelAgency/trips/" + id);
+    return this.http.get<Trip>(this.baseUrl + "trips/" + id);
   }
   
   addTrip(trip: Trip) {
-    return this.http.post(this.baseUrl + "/travelAgency/trips/add", trip, httpOptions);
+    return this.http.post(this.baseUrl + "trips/add", trip, httpOptions);
   }
 
 }
