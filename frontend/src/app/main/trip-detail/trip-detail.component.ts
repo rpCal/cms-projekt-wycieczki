41e7676cfc14136abe5f12fc0ac8750f6d5475e3
Jan Zawadka <janzavvadka@gmail.com>
2019-01-27 12:01:24 +0100

    trip detail improvment

--- frontend/src/app/main/trip-detail/trip-detail.component.ts
+++ frontend/src/app/main/trip-detail/trip-detail.component.ts
@@ -1,6 +1,10 @@
+import { RezerwationComponent } from './../rezerwation/rezerwation.component';
 import { SharedTripService } from './../../service-shared-trip/shared-trip.service';
 import { Trip } from './../../model/trip';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { MatDialog, MatDialogConfig } from '@angular/material';
+
 
 @Component({
   selector: 'app-trip-detail',
@@ -10,10 +14,30 @@ import { Component, OnInit, Input } from '@angular/core';
 export class TripDetailComponent implements OnInit {
   trip: Trip;
 
-  constructor(private sharedTrip: SharedTripService) { }
+  constructor(private sharedTrip: SharedTripService,
+              private location: Location,
+              private dialog: MatDialog) { }
 
   ngOnInit() {
     this.trip = this.sharedTrip.trip;
+    if(this.trip == null){
+      this.backToPrevious();
+    }
   }
+
+  backToPrevious() {
+      this.location.back();
+  }
+
+
+  openDialog() {
+
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = false;
+    dialogConfig.autoFocus = true;
+
+    this.dialog.open(RezerwationComponent, dialogConfig);
+}
 
 }
