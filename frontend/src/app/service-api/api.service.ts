fdb2e63a8fc1eaebad9f408c60d1c0131291bb4e
Robert Całka <calka.rob@gmail.com>
2019-01-31 05:17:53 +0100

    up docs

--- frontend/src/app/service-api/api.service.ts
+++ frontend/src/app/service-api/api.service.ts
@@ -16,6 +16,31 @@ export class ApiService {
   private baseUrl = 'https://pjatk-travel-agency.herokuapp.com/';
   constructor(private http: HttpClient) { }
 
+
+/**
+ * DOSTEPNE API
+ * 
+ * - Pobieranie listy wycieczek wraz z filtrowaniem, sortowaniem, paginacja 
+ * (plus filtrowanie po id, Name, City, Date, Price, AvaiableNumberOfPlaces)
+ * URL: GET /public/Trip?limit=3&skip=0&sort_by_field=Price&sort_by_order=-1&where_Date=2019-12-20
+ * 
+ * 
+ * - LOGOWANIE:
+ * URL POST /auth/login
+ * PAYLOAD: {Email, Password} 
+ * 
+ * 
+ * - REJESTRACJA
+ * URL POST /auth/register
+ * PAYLOAD { Email, Password, FirstName, LastName}
+ * 
+ * 
+ * - POBIERZ AKTUALNEGO USERA
+ * URL GET /auth/profile
+ * PAYLOAD trzeba przekazac JWT token z logowania
+*/
+  
+
   login(username, password){
     return this.http.post<any>(this.baseUrl + `users/authenticate`, { username, password });
   }
